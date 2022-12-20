48f992d6705d68f33570257e540d32545890cacf
vlad-vavshko <vvavshko@hitachisolutions.com>
2022-12-20 11:43:18 +0200

    create handler for /start command

--- bot_files/markup.py
+++ bot_files/markup.py
@@ -3,4 +3,16 @@ from telebot import types
 class Markups:
 
     def __init__(self):
-        self.main = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        # markups
+        self.main = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+
+        # buttons
+        self.add_new_user_btn = types.KeyboardButton("📝 Додати запис")
+        self.delete_user_btn = types.KeyboardButton("❌Видалити запис")
+        self.get_all_users_info_btn = types.KeyboardButton("📖 Переглянути записи")
+
+
+
+
+        # add buttons to marup
+        self.main.add(self.add_new_user_btn, self.delete_user_btn, self.get_all_users_info_btn)
